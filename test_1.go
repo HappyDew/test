9314025ff3a61282cc3b8b8be3e6add26e1c5c62
HappyDew <mygpec@mail.ru>
2018-07-07 17:17:16 +0900

    объявили переменную

--- test_1.go
+++ test_1.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		os.Exit(1)
-	}
-	fmt.Println("It's over ", os.Args[1])
+	var power int
+	power = 9000
+	fmt.Println("It's over \n", power)
 }
