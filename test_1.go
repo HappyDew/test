c1060445c6c9dee234bd15c9348398130b544d2d
HappyDew <mygpec@mail.ru>
2018-07-09 18:18:17 +0900

    а чо не пашет? Оо

--- test_1.go
+++ test_1.go
@@ -4,4 +4,12 @@ import (
 	"fmt"
 )
 
-goku := Saiyan{"Goku",9000}
+func main() {
+	goku := Saiyan{"Goku", 9000}
+	Super(goku)
+	fmt.Println(goku.Power)
+}
+
+func Super(s Saiyan) {
+	s.Power += 10000
+}
