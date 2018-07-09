af06ca3b98ab0f7dc80a869643aad620147510e4
HappyDew <mygpec@mail.ru>
2018-07-09 23:06:24 +0900

    создал простой цикл

--- test_1.go
+++ test_1.go
@@ -5,11 +5,8 @@ import (
 )
 
 func main() {
-	fmt.Print("Enter a number:")
-	var input float64
-	fmt.Scanf("%f", &input)
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
 
-	output := input * (1 / 3.2808)
-
-	fmt.Println(output)
 }
