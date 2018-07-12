d33b78dbb45e056af90af2468b3a6f5ae04836e0
HappyDew <mygpec@mail.ru>
2018-07-12 20:15:57 +0900

    всё

--- test_1.go
+++ test_1.go
@@ -1,22 +1,62 @@
+// package main
+//
+// import (
+// 	"fmt"
+// 	"os"
+// )
+//
+// func main() {
+// 	dir, err := os.Open(".")
+// 	if err != nil {
+// 		return
+// 	}
+// 	defer dir.Close()
+//
+// 	fileInfos, err := dir.Readdir(-1)
+// 	if err != nil {
+// 		return
+// 	}
+// 	for _, fi := range fileInfos {
+// 		fmt.Println(fi.Name())
+// 	}
+// }
+
+// package main
+//
+// import (
+// 	"fmt"
+// 	"os"
+// 	"path/filepath"
+// )
+//
+// func main() {
+// 	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+// 		fmt.Println(path)
+// 		return nil
+// 	})
+// }
+
 package main
 
 import (
 	"fmt"
-	"os"
+	"sync"
+	"time"
 )
 
 func main() {
-	dir, err := os.Open(".")
-	if err != nil {
-		return
-	}
-	defer dir.Close()
+	m := new(sync.Mutex)
 
-	fileInfos, err := dir.Readdir(-1)
-	if err != nil {
-		return
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			m.Lock()
+			fmt.Println(i, "start")
+			time.Sleep(time.Second)
+			fmt.Println(i, "end")
+			m.Unlock()
+		}(i)
 	}
-	for _, fi := range fileInfos {
-		fmt.Println(fi.Name())
-	}
+
+	var input string
+	fmt.Scanln(&input)
 }
