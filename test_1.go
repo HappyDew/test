73e6610222550100641733740581686cec8a36f3
HappyDew <mygpec@mail.ru>
2018-07-12 18:29:53 +0900

    Ыыыыыааааа

--- test_1.go
+++ test_1.go
@@ -2,33 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"golang-book/test/test.git/math"
 )
 
-func pinger(c chan<- string) {
-	for i := 0; ; i++ {
-		c <- "ping"
-	}
-}
-func printer(c <-chan string) {
-	for {
-		msg := <-c
-		fmt.Println(msg)
-		time.Sleep(time.Second * 1)
-	}
-}
-func ponger(c chan string) {
-	for i := 0; ; i++ {
-		c <- "pong"
-	}
-}
 func main() {
-	var c chan string = make(chan string)
-
-	go pinger(c)
-	go ponger(c)
-	go printer(c)
-
-	var input string
-	fmt.Scanln(&input)
+	xs := []float64{1, 2, 3, 4}
+	avg := math.Average(xs)
+	fmt.Println(avg)
 }
