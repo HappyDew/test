201fb56bc5625289d1e7749ccbd57f852b4b2024
HappyDew <mygpec@mail.ru>
2018-07-12 22:18:32 +0900

    выводит папки с файлами

--- test_1.go
+++ test_1.go
@@ -36,27 +36,102 @@
 // 	})
 // }
 
+// package main
+//
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
+//
+// func main() {
+// 	m := new(sync.Mutex)
+//
+// 	for i := 0; i < 10; i++ {
+// 		go func(i int) {
+// 			m.Lock()
+// 			fmt.Println(i, "start")
+// 			time.Sleep(time.Second)
+// 			fmt.Println(i, "end")
+// 			m.Unlock()
+// 		}(i)
+// 	}
+//
+// 	var input string
+// 	fmt.Scanln(&input)
+// }
+
 package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
+	"os"
+	"path"
+	"sort"
 )
 
-func main() {
-	m := new(sync.Mutex)
+type Counter struct {
+	dirs  int
+	files int
+}
 
-	for i := 0; i < 10; i++ {
-		go func(i int) {
-			m.Lock()
-			fmt.Println(i, "start")
-			time.Sleep(time.Second)
-			fmt.Println(i, "end")
-			m.Unlock()
-		}(i)
+func (counter *Counter) index(path string) {
+	stat, _ := os.Stat(path)
+	if stat.IsDir() {
+		counter.dirs += 1
+	} else {
+		counter.files += 1
+	}
+}
+
+// func (counter *Counter) output() string {
+// 	return fmt.Sprintf("\n%d directories, %d files", counter.dirs, counter.files)
+// }
+
+func dirnamesFrom(base string) []string {
+	file, err := os.Open(base)
+	if err != nil {
+		fmt.Println(err)
 	}
 
-	var input string
-	fmt.Scanln(&input)
+	names, _ := file.Readdirnames(0)
+	file.Close()
+
+	sort.Strings(names)
+	return names
+}
+
+func tree(counter *Counter, base string, prefix string) {
+	names := dirnamesFrom(base)
+
+	for index, name := range names {
+		if name[0] == '.' {
+			continue
+		}
+		subpath := path.Join(base, name)
+		counter.index(subpath)
+
+		if index == len(names)-1 {
+			fmt.Println(prefix+"└──", name)
+			tree(counter, subpath, prefix+"    ")
+		} else {
+			fmt.Println(prefix+"├──", name)
+			tree(counter, subpath, prefix+"│   ")
+		}
+	}
+}
+
+func main() {
+	var directory string
+	if len(os.Args) > 1 {
+		directory = os.Args[1]
+	} else {
+		directory = "."
+	}
+
+	counter := new(Counter)
+	fmt.Println(directory)
+
+	tree(counter, directory, "")
+	//fmt.Println(counter.output())
 }
